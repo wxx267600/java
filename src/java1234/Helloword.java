870628de00351558b24f27daaaccf0211c44fa76
wxx <973038754@qq.com>
2020-02-26 09:36:29 +0800

    two

--- src/java1234/Helloword.java
+++ src/java1234/Helloword.java
@@ -3,6 +3,7 @@ package java1234;
 public class Helloword {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("hello everyone");
 
     }
 }
